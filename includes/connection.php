f114aca195503c9018d60ae25fc32956070470b7
RamandeepChamba <ramanchamba98@gmail.com>
2019-07-19 13:50:30 +0530

    Setup mysql db

--- includes/connection.php
+++ includes/connection.php
@@ -1,8 +1,27 @@
 <?php
 
+// Environment
+if (getenv('DATABASE_URL') 
+	&& $_SERVER['SERVER_ADDR'] !== '127.0.0.1') 
+{
+	$url = parse_url(getenv('DATABASE_URL'));
+	$server = $url['host'];
+	$username = $url['user'];
+	$password = $url['pass'];
+	$db = substr($url['path'], 1);
+}
+else {
+	$server = 'localhost';
+        $username = 'blogdb';
+        $password = 'blogdb';
+        $db = 'blogdb';
+}
+
 // Establish connection to db
-$pdo = new PDO('mysql:host=localhost;
-  dbname=blogdb;charset=utf8',
-  'blogdb',
-  'blogdb');
+$pdo = new PDO("mysql:host=$server;
+  dbname=$db;charset=utf8",
+  $username,
+  $password);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+unset($url, $server, $username, $password, $db);
